97343ea62126b8baf581bfafa3edf86ca87377a5
Mike <michaelstennett@hotmail.com>
2018-09-03 16:20:01 -0400

    scrollbars

--- RunningGame.js
+++ RunningGame.js
@@ -580,7 +580,7 @@ GameOverScreen.prototype.onKeyUp = function(currentEvent) {
 	}
 };
 
-var splashScreen = new GameScreen("Splash Screen 2");
+var splashScreen = new GameScreen("Splash Screen 1");
 var menuScreen = new GameScreen("Menu Screen");
 var runScreen = new RunScreen("Run");
 var gameOverScreen = new GameOverScreen("Game Over");
