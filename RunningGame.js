7b59894b538402490bf27bc8db77b5f361185623
Mike <michaelstennett@hotmail.com>
2018-09-05 19:43:02 -0400

    up

--- RunningGame.js
+++ RunningGame.js
@@ -594,7 +594,7 @@ GameOverScreen.prototype.onKeyUp = function(currentEvent) {
 	}
 };
 
-var splashScreen = new GameScreen("Splash Screen 5.8");
+var splashScreen = new GameScreen("Splash Screen 3.8");
 var menuScreen = new GameScreen("Menu Screen");
 var runScreen = new RunScreen("Run");
 var gameOverScreen = new GameOverScreen("Game Over");
